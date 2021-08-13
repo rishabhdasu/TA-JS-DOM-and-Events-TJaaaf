06f7f060f0c1711654ba00abc43af6ae163938d7
rishabhdasu <rishabhdasu@gmail.com>
2021-08-13 18:37:19 +0530

    add

--- block-BJaaes/code/script.js
+++ block-BJaaes/code/script.js
@@ -1,48 +1,77 @@
 let form = document.querySelector(`form`);
 
-let error = "";
-let emailError = "";
-let phoneError = "";
-let passwordError = "";
-let validEmail = `@`;
+let errorMessage = {};
 
-let userNumber = /\[0-9]/g;
+function displayError(name) {
+  let elm = form.elements[name];
+  elm.nextElementSibling.innerText = errorMessage[name];
+  elm.parentElement.classList.add(`error`);
+}
+
+function displaySuccess(name) {
+  let elm = form.elements[name];
+  elm.nextElementSibling.innerText = "";
+  errorMessage[name] =  "";
+  elm.parentElement.classList.add(`error`);
+  elm.parentElement.classList.add(`success`);
+}
+
 
 function handleClick(event) {
     event.preventDefault();
-    console.dir(event.target);
+   let elements = event.target.elements;
 
-    let username = event.target.elements.username;
-    let email = event.target.elements.email;
-    let phone = event.target.elements.phone;
-    let password = event.target.elements.password;
-    let passwordConfirm = event.target.elements.password-confirm;
+    const username = elements.username.value;
+    const name = elements.name.value;
+    const email = elements.email.value;
+    const phone = elements.phone.value;
+    const password = elements.password.value;
+    const passwordCheck = elements.passwordCheck.value;
 
-    if(username.value.length < 4){
-      usernameError = `Can't be less than 4 characters`;
-      username.nextElementSibling.innerText = usernameError;
+    if(username.length < 4){
+      errorMessage.username = `Can't be less than 4 characters`;
+      displayError(`username`);
+    } else {
+      displaySuccess(`username`);
     }
-    else if(username.value.match(userNumber)){
-        usernameError = `Can't include numbers`;
-        username.nextElementSibling.innerText = usernameError;  
+
+    if(!isNaN(name)){
+        errorMessage.name = `Can't include numbers`;
+        displayError(`name`);
+      }
+      else {
+        displaySuccess(`name`);
+      }
+
+    if(!email.includes(`@`)){
+      errorMessage.email = `Must contain @`;
+      displayError(`email`);
     }
-    else if(email.value.indexOf(`@`) == -1){
-      emailError = `Must contain @`;
-      email.nextElementSibling.innerText = emailError;
+    else if(!email.length < 6){
+      errorMessage.email = `Can't be less than 6 characters`;
+      displayError(`email`);
     }
-    else if(email.value.length <6){
-      emailError = `Can't be less than 6 characters`;
-      email.nextElementSibling.innerText = emailError;
+    else {
+      displaySuccess(`email`);
     }
-    else if(!isNaN(phone)){
-        phoneError = `Must be numbers`;
+   
+    if(isNaN(phone)){
+        errorMessage.phone = `Must be numbers`;
+        displayError(`phone`);
     }
-    else if(password.value !== passwordConfirm){
-        passwordError = `Password doesn't match`
-        passwordConfirm.nextElementSibling.innerText = emailError;
+    else {
+      displaySuccess(`phone`);
     }
+
+    if(password !== passwordCheck){
+        errorMessage.password = `Password doesn't match`;
+        displayError(`password`);
+      }
+      else {
+        displaySuccess(`password`);
+        displaySuccess(`passwordCheck`)
+      }
       
-   alert(`User added successfully`); 
  }
   
 
